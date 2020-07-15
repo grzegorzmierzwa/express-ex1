6d07d08e33a4d60389f1b406f492a54f90f15bae
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-07-15 17:13:04 +0200

    Add forms and styles

--- server.js
+++ server.js
@@ -8,34 +8,49 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { name: req.params.name });
+    res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+    res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact');
+    res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info');
+    res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history');
+    res.render('history');
+});
+
+app.post('/contact/send-message', (req, res) => {
+
+    const { author, sender, title, message } = req.body;
+
+    if (author && sender && title && message) {
+        res.render('contact', { isSent: true });
+    }
+    else {
+        res.render('contact', { isError: true });
+    }
+
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
+    res.status(404).send('404 not found...');
 })
 
 app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+    console.log('Server is running on port: 8000');
 });
